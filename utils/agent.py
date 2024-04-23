882b74d8c112ec05acc98ef7db5307b701b49c50
Royisaboy <zhuyanfu0712@gmail.com>
2024-04-23 18:52:42 +0000

    update

--- utils/agent.py
+++ utils/agent.py
@@ -9,29 +9,20 @@ from email.mime.text import MIMEText
 import gspread
 from utils.prompt import PromptGeneration
 import pandas as pd
-# import gdown
+import gdown
 import uuid
 from google.cloud import storage
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
+import io
 
 class Actions(chat):
     def __init__(self, key):
         super().__init__(key)
         self.storage_client = storage.Client.from_service_account_json('gcp_credential.json')
         self.bucket = self.storage_client.bucket("infind")
-
-    def scrape_web(self, url, max_pages):
-        payload = json.dumps({
-            "url": url,
-            "match": f"{url}/**",
-            "maxPagesToCrawl": max_pages,
-            "outputFileName": "output.json",
-            "maxTokens": 2000000
-        })
-        headers = {
-            'Content-Type': 'application/json'
-        }
-        response = request("POST", "http://localhost:3000/crawl", headers=headers, data=payload)
-        return response.json()
 
 
     def extract_email_addresses(self, text):
@@ -41,30 +32,6 @@ class Actions(chat):
             return match.group(0)
         else:
             return match
-
-    def extract_phone_numbers(self, text):
-        pattern = r'\+?\d[\d -]{8,}\d'
-        match = re.search(pattern, text)
-        if match is not None:
-            return match.group(0)
-        else:
-            return match
-
-    def find_email_or_phone(self, text):
-        if text is None:
-            return None  # Return None or handle appropriately if text is None
-        contact = self.extract_email_addresses(text)
-        if contact is None:
-            contact = self.extract_phone_numbers(text)
-        return contact
-    
-    def find_email_or_phone_recursively(self, scrape_web):
-        contacts = []
-        for r in scrape_web:
-            contact = self.find_email_or_phone(r["html"])
-            if contact is not None:
-                contacts.append(contact)
-        return ", ".join(list(set(contacts)))
 
     def send_email(self, password, from_email, to_email, subject, supplier_summary_collections):
         df_html = pd.DataFrame(supplier_summary_collections).to_html()
@@ -98,30 +65,8 @@ class Actions(chat):
         smtp.sendmail(from_email, to_email, message.as_string())
         smtp.quit()
 
-    def search_contact(self, query):
-        url = f"https://www.googleapis.com/customsearch/v1?key=AIzaSyD6hSd8BCa-VYtNCyIMFFgIOD93xsl_HEc&cx=84249567808554f8e&q={query}"
-        search_reulsts = request("GET", url).json()
-        if "items" not in search_reulsts:
-            return ""
-        contact_url = search_reulsts["items"][0]["link"]
-        crawl_contact_results = self.scrape_web(contact_url, 3)
-        contact = self.find_email_or_phone_recursively(crawl_contact_results)
-        if contact is not None:
-            return contact
-        else:
-            return ""
         
-    def search_product_link(self, query):
-        try:
-            url = f"https://www.googleapis.com/customsearch/v1?key=AIzaSyD6hSd8BCa-VYtNCyIMFFgIOD93xsl_HEc&cx=84249567808554f8e&q={query}"
-            search_reulsts = request("GET", url).json()
-            return search_reulsts["items"][0]["link"]
-        except Exception:
-            return ""
-        
-    def check_recommendations_accuracy(self, description, recommendations):
-        p = PromptGeneration()
-        prompt = p.get_recommendation_accuracy(description, recommendations)
+    def check_recommendations_accuracy(self, prompt):
         product_recommendations = self.get_response(prompt)
         if "results" in product_recommendations or "products" in product_recommendations:
             product_recommendations = list(product_recommendations.values())[0]
@@ -129,26 +74,11 @@ class Actions(chat):
             product_recommendations = [product_recommendations]
         return product_recommendations
     
-    def fetch_sheet_latest_records(self, gcp_credential_dir):
+    def fetch_sheet_all_records(self, gcp_credential_dir):
         gc = gspread.service_account(filename=gcp_credential_dir)
         sht2 = gc.open_by_url('https://docs.google.com/spreadsheets/d/1EODc6xDvF8vS-R4JH1VleKhxwQlhhhF27fGu9Upd1pg/')
         worksheet = sht2.get_worksheet(0)
-        list_of_dicts = worksheet.get_all_records()
-        with open("data/google_sheet_history.json", "r") as f_in:
-            current_position = json.load(f_in)["current_position"]
-        remaining_list_of_dicts = list_of_dicts[current_position:]
-        
-        modified = []
-        for d in remaining_list_of_dicts:
-            request_id = str(uuid.uuid4())
-            d["request_id"] = request_id
-            modified.append(d)
-            blob = self.bucket.blob(f"inbuy/logs/{request_id}/request.json")
-            blob.upload_from_string(json.dumps(d))
-        new_position = current_position + len(modified)
-        with open("data/google_sheet_history.json", "w") as f_out:
-            json.dump({"current_position": new_position}, f_out)
-        return modified
+        return worksheet.get_all_records()
     
     def generate_supplier_summary(self, request_id, supplier_name, supplier_contact, product_recommendations_str):
         
@@ -167,13 +97,30 @@ class Actions(chat):
         blob = self.bucket.blob(f"inbuy/logs/{request_id}/recommendations.json")
         blob.upload_from_string(json.dumps(supplier_summary))
         return supplier_summary
-    
-    def prune_scraped_reuslts(self, s):
-        while len(json.dumps(s)) > 32000:
-            if len(s) == 1:
-                first_element = s[0]
-                first_element["html"] = first_element["html"][:32000]
-                s = [first_element]
-            else:
-                s = s[:-1]
-        return s
+
+    def download_excel_file(self, real_file_id):
+        creds = service_account.Credentials.from_service_account_json('gcp_credential.json')
+
+        try:
+            # create drive api client
+            service = build("drive", "v3", credentials=creds)
+
+            # pylint: disable=maybe-no-member
+            request = service.files().get_media(fileId=real_file_id)
+            file = io.BytesIO()
+            downloader = MediaIoBaseDownload(file, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                print(f"Download {int(status.progress() * 100)}.")
+            # Use BytesIO to convert bytes to a file-like object that pandas can read
+            data = io.BytesIO(file.getvalue())
+
+            # Read the Excel file from the file-like object
+            return pd.read_excel(data).to_dict("records")
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+            return None
+
+
+
--- utils/gpt4.py
+++ utils/gpt4.py
@@ -1,10 +1,17 @@
 from openai import OpenAI
 import json
-
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.llms.openai import OpenAI as llama_index_openai
+from llama_index.core import Settings
+from llama_index.vector_stores.pinecone import PineconeVectorStore
+import os
+from pinecone import Pinecone, ServerlessSpec
 class chat:
 
     def __init__(self, key):
         self.client = OpenAI(api_key=key)
+        Settings.llm = llama_index_openai(temperature=0.1, response_format={"type": "json_object"}, model="gpt-4-turbo")
+        self.pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 
     def get_response(self, text_prompt, urls=None):
         if urls is None:
@@ -22,3 +29,10 @@ class chat:
         latest_message = chat_response.choices[0].message
         return json.loads(latest_message.content)
     
+    def get_rga_response(self, text_prompt, index_name):
+        index = self.pc.Index(index_name)
+        vector_store = PineconeVectorStore(pinecone_index=index)
+        loaded_index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+        query_engine = loaded_index.as_query_engine()
+        response = query_engine.query(text_prompt)
+        return json.loads(response.response)
